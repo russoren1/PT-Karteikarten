c9101056e717a6f240189626539215b29e921d74
russoren1 <russoren@students.zhaw.ch>
2026-05-03 00:09:31 +0200

    Mache Navbar responsiv mit Bootstrap Collapse

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,17 +1,30 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-secondary">
-	<div class="container d-flex flex-wrap gap-3">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark border-bottom border-secondary">
+	<div class="container">
 		<a class="navbar-brand fw-semibold" href="/">PT Karteikarten</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#mainNavigation"
+			aria-controls="mainNavigation"
+			aria-expanded="false"
+			aria-label="Navigation umschalten"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-		<ul class="navbar-nav flex-row flex-wrap gap-3 ms-auto">
-			<li class="nav-item">
-				<a class="nav-link px-0" href="/">Home</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link px-0" href="/stapel">Dashboard</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link px-0" href="/stapel">Stapel</a>
-			</li>
-		</ul>
+		<div class="collapse navbar-collapse" id="mainNavigation">
+			<ul class="navbar-nav ms-auto mb-2 mb-md-0">
+				<li class="nav-item">
+					<a class="nav-link" href="/">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/stapel">Dashboard</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/stapel">Stapel</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </nav>
