cdc5b9e024712f870a4515ab4707f4fe1951eca9
russoren1 <russoren@students.zhaw.ch>
2026-05-03 12:08:13 +0200

    korrigiere KI generierte Navbar, durch einfache Bootstrap Navbar + Hinzufügen des Logos in die Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import '../app.css';
-	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,7 +8,24 @@
 	<link rel="icon" href="/favicon.svg" />
 </svelte:head>
 
-<Navbar />
+<nav class="navbar navbar-expand-lg bg-black fw-bold" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+      <img class="navbar-logo" src="/logo.png" alt="PT Karteikarten" />
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="/stapel">Stapel</a>
+        <a class="nav-link" href="/dashboard">Dashboard</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
 
 <main class="app-shell py-5">
 	{@render children()}
