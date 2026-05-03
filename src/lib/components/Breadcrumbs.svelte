094094e3eabbda5580cf2f4e143d9be5a46c34a8
russoren1 <russoren@students.zhaw.ch>
2026-05-03 23:24:17 +0200

    Erstelle wiederverwendbare Bootstrap Breadcrumb Komponente für bessere Orientierung in tiefen Stapel und Karten Routen

---
+++ src/lib/components/Breadcrumbs.svelte
@@ -0,0 +1,28 @@
+<script>
+	let { items = [] } = $props();
+</script>
+
+{#if items.length}
+	<nav aria-label="Breadcrumb">
+		<ol class="breadcrumb mb-4">
+			{#each items as item, index}
+				<li
+					class="breadcrumb-item text-light"
+					class:active={index === items.length - 1}
+					aria-current={index === items.length - 1 ? 'page' : undefined}
+				>
+					{#if item.href && index !== items.length - 1}
+						<a
+							class="link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
+							href={item.href}
+						>
+							{item.label}
+						</a>
+					{:else}
+						{item.label}
+					{/if}
+				</li>
+			{/each}
+		</ol>
+	</nav>
+{/if}
